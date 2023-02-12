cb6441b8dc92d29fe1063a6049d45adb315c8032
MartinBST <martin.kanev@bst.bg>
2023-02-12 13:54:39 +0200

    init commit

---
+++ src/test/java/com/softuni/battleshipsapp/BattleshipsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.battleshipsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
